20053d6bfb9a70e0afaa090837a000e76a16f168
RubenHUBGIT <estebanb.ruben5@gmail.com>
2026-07-04 17:55:56 +0200

    Fix mobile filter overflow, improve clear-stars button, bump SW cache to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lecturas-v2';
+const CACHE = 'lecturas-v3';
 const ASSETS = ['./lectura.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
